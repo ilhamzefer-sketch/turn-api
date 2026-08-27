8734c2e6d7487e94de27603cbed96f2f62bb531f
Camal Cavadov <camalcavadovwork@gmail.com>
2026-08-27 15:28:53 +0400

    test: align postgres migration expectation

--- src/test/java/az/turn/api/UserPostgresIntegrationTests.java
+++ src/test/java/az/turn/api/UserPostgresIntegrationTests.java
@@ -50,7 +50,7 @@ class UserPostgresIntegrationTests {
 
     @Test
     void appliesAllMigrationsAndEnforcesUniquePhone() {
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("26");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("27");
         userRepository.saveAndFlush(activeUser("+994505556677"));
 
         assertThrows(
