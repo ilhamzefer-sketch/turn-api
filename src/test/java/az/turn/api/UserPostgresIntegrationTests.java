c85be79abdcc3e45e8e4dfcc73a1b6f5d692568e
Camal Cavadov <camalcavadovwork@gmail.com>
2026-09-01 10:19:48 +0400

    test: expect latest support migration

--- src/test/java/az/turn/api/UserPostgresIntegrationTests.java
+++ src/test/java/az/turn/api/UserPostgresIntegrationTests.java
@@ -51,7 +51,7 @@ class UserPostgresIntegrationTests {
 
     @Test
     void appliesAllMigrationsAndEnforcesUniquePhone() {
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("35");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("37");
         userRepository.saveAndFlush(activeUser("+994505556677"));
 
         assertThrows(
